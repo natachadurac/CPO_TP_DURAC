b298dfd6691c7097b4efa1f7d825d32efd9ca2b4
Natacha Durac <natacha.durac@epfedu.fr>
2024-09-20 11:15:56 +0200

    Création du projet

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP0 Exercice 1
+ * Durac Natacha
+ *Septembre 2024
  */
 package exo2;
 
@@ -14,7 +15,20 @@ public class Exo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+        nb=5;
+        result=0;
+        // Addition des nb premiers entiers
+        ind=1;
+        while (ind <= nb) {
+        result=result+ind;
+}
+// Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result);
     }
     
 }
